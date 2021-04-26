954e82b4ba7fc6e82ebcd210a83abb602fc1e293
fernandovincent <s21810081@student.unklab.ac.id>
2021-04-26 23:38:04 +0800

    commitSlicingSignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Button, Gap, Header, TextInput} from '../../components';
 
-const SignIn = () => {
+const SignIn = ({navigation}) => {
   return (
-    <View>
-      <Text>Sign In Pages</Text>
+    <View style={styles.page}>
+      <Header title="Sign In" />
+      <View style={styles.contentWrapper}>
+      <TextInput title="Email Address" placeholder="Type your email address" />
+      <Gap height={16} />
+      <TextInput title="Password" placeholder="Type your password" />
+      <Gap height={24} />
+      <Button title="Sign In" />
+      <Gap height={12} />
+      <Button title="Create New Account" color="#8D92A3" textColor="white" onPress={() => navigation.navigate('SignUp')} /> 
+      </View>
     </View>
-  )
-}
+  );
+};
 
 export default SignIn;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  contentWrapper: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingHorizontal: 24,
+    paddingTop: 26,
+    marginTop: 24,
+  },
+});
